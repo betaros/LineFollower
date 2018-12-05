5f08859b15354dcad691c3dac14bcbfb26cc5c51
Jan-T. Brinkmann <ppp1pp1p1@googlemail.com>
2018-12-05 15:53:01 +0100

    improve detection of new (white) road style

--- src/Main.py
+++ src/Main.py
@@ -2,6 +2,7 @@
 import math
 
 import cv2
+from cv2.cv2 import INTER_LINEAR
 
 
 def force_odd_number(num):
@@ -14,93 +15,122 @@ def force_odd_number(num):
 def ceil(num):
     return int(math.ceil(num))
 
+
+# parameters
+width = 300
+height = 200
+road_darkest_gray = 120
+road_edge_threshold = 10
+road_edge_threshold_is_upper_limit = True
+road_detect_max_steps = 6
+
+# calculated params
+width_half = int(width / 2)
+typical_road_width = width_half
+min_road_width = ceil(typical_road_width / 4)
+road_detect_step_size = ceil(height / 20)
+threshold_multiplier = road_edge_threshold_is_upper_limit * 2 - 1
+
+median_blur_kernel = force_odd_number(int(width / 20))
+gaussian_blur_kernel = force_odd_number(int(width / 30))
+
+# constants
+TO_LEFT = -1
+TO_RIGHT = +1
+
+
 def detect_line():
-    # parameters
-    width = 300
-    height = 200
-    road_darkest_gray = 100
-    road_edge_threshold = 10
-
-    # calculated params
-    width_half = int(width / 2)
-    typical_road_width = width_half
-    min_road_width = ceil(typical_road_width / 4)
-    road_detect_step_size = ceil(height / 10)
-
-    median_blur_kernel = force_odd_number(int(width / 10))
-    gaussian_blur_kernel = force_odd_number(int(width / 30))
-
     # camera = cv2.VideoCapture(0)
+    # while True:
+    # ret_val, image = camera.read()
+    # image = cv2.rotate(image, ROTATE_180)
+
+    # image = cv2.imread("../images/sample-straight.jpg")
+    image = cv2.imread("../images/new-curve.jpg")
+    image = cv2.resize(image, (width, height), interpolation=INTER_LINEAR)
+    # out = image
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # kernel = np.ones((3, 3), np.uint8)
+    # image = cv2.erode(image, kernel, iterations=5)
+    # image = cv2.dilate(image, kernel, iterations=8)
+    image = cv2.medianBlur(image, median_blur_kernel, 0)
+    image = cv2.inRange(image, 0, road_darkest_gray)
+    out = image
+    # image = cv2.GaussianBlur(image, (gaussian_blur_kernel, gaussian_blur_kernel), 0)
+
+    # Line detection
+    # image = cv2.Canny(image, 0, 0, apertureSize=3)
+
+    # find road boundaries at bottom of image
+    bottom = height - 1
+    top = 20  # height-1 - road_detect_step_size * road_detect_max_steps
+    left_edge = right_edge = width_half
+    # cv2.line(out, (left_edge, bottom), (right_edge, bottom), 100)
+
+    old_y = bottom
+    print("bottom=%d, top=%d, step=%d" % (bottom, top, road_detect_step_size))
+    # for y in range(bottom, 0, road_detect_step_size):
+    left_edge = right_edge = None
+
+    for y in range(bottom, top, -road_detect_step_size):
+        # print("checking y=%s, left=%s, right=%s" % (y, left_edge, right_edge))
+        new_left_edge = detect_edge(image, left_edge or width_half, y, TO_LEFT)
+        new_right_edge = detect_edge(image, right_edge or width_half, y, TO_RIGHT)
+
+        if new_left_edge is None or new_right_edge is None:
+            continue
+
+        # if right_edge - left_edge < min_road_width:
+        #     break
+
+        middle = ceil((new_left_edge + new_right_edge) / 2)
+        print("middle=%d; delta=%d" % (middle, middle - width_half))
+        if left_edge is None and right_edge is None:
+            # lowest road bounds
+            # report delta
+            pass
+        else:
+            old_middle = ceil((left_edge + right_edge) / 2)
+            cv2.line(out, (left_edge - 5, old_y), (new_left_edge - 5, y), 100)
+            cv2.line(out, (right_edge + 5, old_y), (new_right_edge + 5, y), 100)
+
+            cv2.line(out, (old_middle, old_y), (middle, y), 100)
+        old_y = y
+
+        left_edge = new_left_edge
+        right_edge = new_right_edge
+
+    middle = int((right_edge + left_edge) / 2)
+    cv2.line(out, (middle, old_y - 5), (width_half, old_y + 5), 50)
+
+    # out = cv2.resize(out, (width*10, height*10))
+    cv2.imshow("original with line", out)
+
     while True:
-        # ret_val, image = camera.read()
-        # image = cv2.imread("../images/sample-curve.jpg")
-        image = cv2.imread("../images/sample-straight.jpg")
-        image = cv2.resize(image, (width, height))
-        out = image
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        # kernel = np.ones((3, 3), np.uint8)
-        # image = cv2.erode(image, kernel, iterations=5)
-        # image = cv2.dilate(image, kernel, iterations=8)
-        image = cv2.medianBlur(image, median_blur_kernel, 0)
-        image = cv2.inRange(image, 0, road_darkest_gray)
-        # image = cv2.GaussianBlur(image, (gaussian_blur_kernel, gaussian_blur_kernel), 0)
-        out = image
-
-        # Line detection
-        # image = cv2.Canny(image, 0, 0, apertureSize=3)
-
-        # find road boundaries at bottom of image
-        bottom = height-1
-        top = height-1 - road_detect_step_size*3
-        left_edge = right_edge = width_half
-        # cv2.line(out, (left_edge, bottom), (right_edge, bottom), 100)
-
-        old_y = bottom
-        # for y in range(bottom, 0, -road_detect_step_size):
-        for y in range(bottom, top, -road_detect_step_size):
-            old_left_edge = left_edge
-            old_right_edge = right_edge
-            left_edge = detect_edge(image, road_edge_threshold, left_edge, y, -1)
-            right_edge = detect_edge(image, road_edge_threshold, right_edge, y, +1)
-
-            if right_edge - left_edge < min_road_width:
-                break
-
-            cv2.line(out, (old_left_edge-5, old_y), (left_edge-5, y), 100)
-            cv2.line(out, (old_right_edge+5, old_y), (right_edge+5, y), 100)
-            old_y = y
-
-        middle = int((right_edge + left_edge) / 2)
-        cv2.line(out, (middle, y), (width_half, y+5), 50)
-
-
-
-
-
-        # out = cv2.resize(out, (width*10, height*10))
-        cv2.imshow("original with line", out)
-
         # Exit when Escape is pressed
         if cv2.waitKey(1) == 27:
             break
 
 
-def detect_edge(image, threshold, x, y, direction):
-    width = len(image[0])
+def is_on_line(image, x, y):
+    return image[y, x] * threshold_multiplier > road_edge_threshold * threshold_multiplier
 
-    if image[y, x] > threshold:
-        while image[y, x] > threshold:
+
+def detect_edge(image, x, y, direction):
+    if is_on_line(image, x, y):
+        while is_on_line(image, x, y):
+            # print("> %d / %d" % (x, width))
             x += direction
-            if 0 > x or x > width:
-                return x - direction
-        return x
-
+            if not (0 <= x < width):
+                return None
+        return x - direction
     else:
-        while image[y, x] <= threshold:
+        while not is_on_line(image, x, y):
+            # print("< %d / %d" % (x, width))
             x -= direction
-            if 0 > x or x > width:
-                return x + direction
+            if not (0 <= x < width):
+                return None
         return x + direction
 
 
